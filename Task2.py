522430585338f38bdd94c0ce5b0abf8f1e80ccc7
Ssuubi-Grace <muwongegrace2@gmail.com>
2026-04-13 11:01:27 +0300

    improve: use HEAD request and simplify code by removing unnecessary comments

--- Task2.py
+++ Task2.py
@@ -7,34 +7,23 @@ file_path = 'Task 2 - Intern.csv'
 
 def print_status_codes(file_path):
     """
-    Reads a list of URLs from a CSV file and shows their status codes.
+    Read URLs from CSV and print their  HTTP status codes.
     Format: (STATUS_CODE) URL 
     """
     
-    #opening the CSV file with UTF-8 encoding to handle non-ASCIII text.
     with open(file_path, mode = 'r', encoding = 'utf-8') as file:
-        
-        #reading the CSV rows as dictionaries so columns can be accessed by their header names
         url_reader = csv.DictReader(file) 
-
-        #getting the first column name as the key for accessing URLs   
         url_key = url_reader.fieldnames[0]  
         
         for row in url_reader:
-            url = row[url_key].strip() #removing any accidental spaces from the URL
+            url = row[url_key].strip()
 
-           
-            #Fetching the URL with a timeout of and printing it's status code.
             try:
-                response = requests.get(url, timeout=10)
-        
+                response = requests.head(url, timeout=10)
                 print(f"({response.status_code}) {url}")  
             
-            #handling request exceptions and printing an error message with the URL. 
             except requests.exceptions.RequestException as err: 
-                print(f"(Error) {err} {url}")
-                
-file_path = 'Task 2 - Intern.csv'
+                print(f"(Error) {url}")
 
 if __name__ == "__main__":
     print_status_codes(file_path)
